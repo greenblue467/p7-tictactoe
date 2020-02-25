6030469753ba45f32cb3b0adfb41c88e26a88113
greenblue467 <greenblue467@hotmail.com>
2020-02-25 18:20:47 +0800

    Updates

---
+++ precache-manifest.9780d33659933895329123254c900791.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b4975d0f3027a8a0ea884c6f3a29835",
+    "url": "/p7-tictactoe/index.html"
+  },
+  {
+    "revision": "2a38fad7b2acd350ee39",
+    "url": "/p7-tictactoe/static/css/main.b187e53d.chunk.css"
+  },
+  {
+    "revision": "ad42f578215574a08ddc",
+    "url": "/p7-tictactoe/static/js/2.3db0d99b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/p7-tictactoe/static/js/2.3db0d99b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a38fad7b2acd350ee39",
+    "url": "/p7-tictactoe/static/js/main.75048287.chunk.js"
+  },
+  {
+    "revision": "e3d61e72791ae7cb88c1",
+    "url": "/p7-tictactoe/static/js/runtime-main.d97135f1.js"
+  }
+]);
